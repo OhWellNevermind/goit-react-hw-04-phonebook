62bde6de517edbc3c58872a2c1fe129b530690ec
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-08-30 19:46:14 +0300

    on update add to local storage on mount getting data from local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,19 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    if (localStorage.getItem('contacts') !== null)
+      this.setState({
+        contacts: JSON.parse(localStorage.getItem('contacts')),
+      });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addNewContact = newContact => {
     const isInContacts = this.state.contacts.filter(contact => {
       return newContact.number === contact.number;
