0b33f875211d240c7dcb0ce383890a3fb52088d4
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-09-08 00:34:13 +0300

    refatored to useState, useEffect, useMemo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,32 @@
-import { Component } from 'react';
 import { ContactsList } from './ContactsList/ContactsList';
 import { NewContactForm } from './NewContactForm/NewContactForm';
 import { nanoid } from 'nanoid';
 import { SearchBar } from './SearchBar/SearchBar';
 import { Container } from '@mui/material';
+import { useEffect, useMemo, useState } from 'react';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    if (localStorage.getItem('contacts') !== null)
-      this.setState({
-        contacts: JSON.parse(localStorage.getItem('contacts')),
-      });
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  useEffect(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) {
+      setContacts(JSON.parse(savedContacts));
     }
-  }
+  }, []);
 
-  addNewContact = newContact => {
-    const isInContacts = this.state.contacts.filter(contact => {
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addNewContact = newContact => {
+    const isInContacts = contacts.filter(contact => {
       return newContact.number === contact.number;
     }).length;
 
@@ -38,48 +34,34 @@ export class App extends Component {
       alert(`${newContact.name} is already in contacts`);
       return;
     }
-
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, { id: nanoid(), ...newContact }],
-    }));
+    setContacts([...contacts, { id: nanoid(), ...newContact }]);
   };
 
-  onSearch = value => {
-    this.setState({ filter: value });
+  const onSearch = value => {
+    setFilter(value);
   };
 
-  onDeleteContact = contactId => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(
-          contacts => contacts.id !== contactId
-        ),
-      };
+  const onDeleteContact = contactId => {
+    setContacts(contacts.filter(contacts => contacts.id !== contactId));
+  };
+
+  const visibleContacts = useMemo(() => {
+    return contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(filter?.toLowerCase());
     });
-  };
+  }, [contacts, filter]);
 
-  render() {
-    const { contacts, filter } = this.state;
-    const visibleContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-
-    return (
-      <Container>
-        <h1>PhoneBook</h1>
-        <NewContactForm
-          addNew={this.addNewContact}
-          setName={this.setName}
-          setNumber={this.setNumber}
-        ></NewContactForm>
-        <h2>Contacts</h2>
-        <SearchBar onSearch={this.onSearch}></SearchBar>
-        <ContactsList
-          onDelete={this.onDeleteContact}
-          contacts={visibleContacts}
-        ></ContactsList>
-        {!visibleContacts.length && <p>There is no contacts</p>}
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <h1>PhoneBook</h1>
+      <NewContactForm addNew={addNewContact}></NewContactForm>
+      <h2>Contacts</h2>
+      <SearchBar onSearch={onSearch}></SearchBar>
+      <ContactsList
+        onDelete={onDeleteContact}
+        contacts={visibleContacts}
+      ></ContactsList>
+      {!visibleContacts.length && <p>There is no contacts</p>}
+    </Container>
+  );
+};
